edcb77a6f6281021d933f760433e65b4ebcb3a9f
stalincrisanto <stalincrisanto@hotmail.com>
2025-12-22 11:43:33 -0500

    feat: enable cors

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,10 @@ import express from "express";
 import clienteRoutes from "./routes/pronobis.routes";
 import pisosRoutes from "./routes/pisos.routes";
 import inmueblesRoutes from "./routes/inmuebles.routes";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/cliente-proyectos", clienteRoutes);
